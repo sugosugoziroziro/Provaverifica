062a5f3dcfbfdb2a9dd479746139e5001e08d80e
SUGO <matte.sirotti2@gmail.com>
2024-02-28 17:10:33 +0100

    Add project files.

---
+++ Provaverifica/Cerchio.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provaverifica
+{
+    internal class Cerchio
+    {
+        public double Raggio { get; set; }
+        public double Circonferenza { get { return 2 * Raggio * Math.PI; } } 
+        public double CalcoloCirc()
+        {
+            return 2 * Raggio * Math.PI;
+        }
+        public Cerchio(double raggio) 
+        { 
+            Raggio= raggio;
+        }
+        public override string ToString()
+        {
+            return $"r={Raggio}";
+        }
+        public static bool TryParse(string s,out Cerchio c)
+        {
+            try 
+            {
+                if (Cerchio.Parse(s).ToString() == s) // "pollo"
+                {
+                    c = Cerchio.Parse(s);
+                    return true;
+                }
+                else
+                {
+                    c = null;
+                    return false;
+                }
+            }
+            catch(Exception e)
+            {
+                c = null;
+                return false;
+            }
+            
+        }
+        public static bool operator ==(Cerchio c1, Cerchio c2)
+        {
+            if (Object.ReferenceEquals(c1,null))
+            {
+                if(Object.ReferenceEquals(c2,null))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+                
+            else if(Object.ReferenceEquals(c2,null)) 
+            {
+                return false;
+            }
+            else
+            {
+                if(c1.Raggio==c2.Raggio)
+                {
+                    return true;
+                }
+                else { return false;}
+            }
+        }
+    
+        public static bool operator !=(Cerchio c1, Cerchio c2)
+        {
+            if(c1==c2)
+            {
+                return false;
+            }
+            else 
+                return true;
+   
+        }
+        public static Cerchio Parse(string s)
+        {
+            string[] a = new string[2];
+            a = s.Split('=');
+
+            return new Cerchio(double.Parse(a[1]));   
+        }
+
+    }
+
+}
---
+++ Provaverifica/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provaverifica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string s = "pollo";
+            Cerchio c;
+            if (Cerchio.TryParse(s, out c))
+
+            {
+                Console.WriteLine(c);
+            }
+
+            else {Console.WriteLine("ERRORE");}
+
+            Cerchio b = new Cerchio(45);
+            Cerchio d = new Cerchio(45);
+            Cerchio b=Cerchio d;
+            if (b == d)
+                { 
+                    Console.WriteLine("sono uguali");
+                }
+            else
+                {
+                    Console.WriteLine("non sono uguali");
+                }
+
+
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
